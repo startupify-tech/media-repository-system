777bd49f30ca8a853cb5ba79f1a3da5232bbae98
amicable-person <62683239+amicable-person@users.noreply.github.com>
2020-04-02 20:11:14 +0530

    Add files via upload

--- src/manage.py
+++ src/manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Mypage.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MyPro.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
